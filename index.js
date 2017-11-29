c7f58f1261447dc18b4c9ff884d62c501f562dd1
l.gil <l.gil@AR-IT10026>
2017-11-29 20:51:27 -0300

    repo to merge

---
+++ index.js
@@ -0,0 +1,48 @@
+AR-IT10026
+
+Windows IP Configuration
+
+
+Ethernet adapter Ethernet 2:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . : globant.com
+
+Ethernet adapter vEthernet (DockerNAT):
+
+   Connection-specific DNS Suffix  . : 
+   Link-local IPv6 Address . . . . . : fe80::1cad:4cf5:c461:8871%7
+   IPv4 Address. . . . . . . . . . . : 10.0.75.1
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . : 
+
+Ethernet adapter vEthernet (HNS Internal NIC):
+
+   Connection-specific DNS Suffix  . : 
+   Link-local IPv6 Address . . . . . : fe80::88cc:2c95:9232:b70%19
+   IPv4 Address. . . . . . . . . . . : 172.29.144.1
+   Subnet Mask . . . . . . . . . . . : 255.255.240.0
+   Default Gateway . . . . . . . . . : 
+
+Ethernet adapter Ethernet:
+
+   Connection-specific DNS Suffix  . : cpe.telecentro.net.ar
+   IPv6 Address. . . . . . . . . . . : 2800:810:464:d10:bdb1:f27a:2593:9b29
+   IPv6 Address. . . . . . . . . . . : 2800:810:464:d10:d681:d7ff:fe77:9ef1
+   Temporary IPv6 Address. . . . . . : 2800:810:464:d10:6846:798e:f585:21b7
+   Temporary IPv6 Address. . . . . . : 2800:810:464:d10:f513:bea5:46b6:1edc
+   Link-local IPv6 Address . . . . . : fe80::bdb1:f27a:2593:9b29%8
+   IPv4 Address. . . . . . . . . . . : 192.168.0.16
+   Subnet Mask . . . . . . . . . . . : 255.255.255.0
+   Default Gateway . . . . . . . . . : fe80::ce0d:ecff:fec3:52c3%8
+                                       192.168.0.1
+
+Wireless LAN adapter Local Area Connection* 2:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . : 
+
+Wireless LAN adapter Wi-Fi:
+
+   Media State . . . . . . . . . . . : Media disconnected
+   Connection-specific DNS Suffix  . : cpe.telecentro.net.ar
